38fb5c2a14e553394b903d9d21980c30d3f8f5cb
trionoaja <trionoaja@gmail.com>
2025-05-06 18:26:52 +0700

    materi baru

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,11 +1,15 @@
 <?php
-defined('BASEPATH') OR exit ('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller{
-
     public function __construct(){
         parent::__construct();
         $this->load->model('User_model');
+    }
+    public function login(){
+        //$this->load->view('templates/header');
+        $this->load->view('auth/login');
+        //$this->load->view('templates/footer');
     }
 
     public function register(){
@@ -13,31 +17,65 @@ class Auth extends CI_Controller{
         $this->load->view('auth/register');
         $this->load->view('templates/footer');
     }
-
     public function process_register(){
         $this->form_validation->set_rules('username','username','required|is_unique[users.username]');
         $this->form_validation->set_rules('password','password','required|min_length[6]');
-        $this->form_validation->set_rules('confirm_password','Konfirmasi password','required|matches[password]');
+        $this->form_validation->set_rules('confirm_password','konfirmasi password','required|matches[password]');
         $this->form_validation->set_rules('role','role','required');
-
-        if ($this->form_validation->run() == FALSE){
+        
+        if($this->form_validation->run() == FALSE){
             $this->load->view('templates/header');
-            $this->load->view('auth/register');
-            $this->load->view('templates/footer');
+			$this->load->view('auth/register');
+			$this->load->view('templates/footer');
         }else{
             $data =[
                 'username'=> $this->input->post('username'),
                 'password'=> password_hash($this->input->post('password'), PASSWORD_DEFAULT),
-                'role'=> $this->input->post('role')
+                'role' => $this->input->post('role')
             ];
 
             if($this->User_model->insert_user($data)){
-                $this->session->set_flashdata('success','Pendaftaran Berhasil silakan login');
+                $this->session->set_flashdata('success','Pendaftaran Berhasil! silakan login');
                 redirect('auth/login');
             }else{
-                    $this->session->set_flashdata('error','Pendaftaran Gagal Coba lagi');
-                    redirect('auth/register');
+                $this->session->set_flashdata('error','Pendaftaran gagal! Coba lagi');
+                redirect('auth/register');
             }
         }
+    }
+    public function process_login() {
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        $user = $this->User_model->check_user($username, $password);
+        if ($user) {
+            $this->session->set_userdata([
+                'user_id' => $user->id,
+                'username' => $user->username,
+                'role' => $user->role,
+                'logged_in' => TRUE
+            ]);
+
+            $this->redirect_by_role($user->role);
+        } else {
+            $this->session->set_flashdata('error', 'Username atau Password salah');
+            redirect('auth/login');
+        }
+    }
+    private function redirect_by_role($role) {
+        switch ($role) {
+            case 'admin':
+                redirect('dashboard');
+                break;
+            case 'user':
+                redirect('dashboard_user');
+                break;
+            default:
+                redirect('auth/login');
+        }
+    }
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect('auth/login');
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -8,13 +8,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css'); ?>">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css'); ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css'); ?>">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -27,10 +27,12 @@
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
-
-      <form action="../../index3.html" method="post">
+      <?php if ($this->session->flashdata('error')): ?>
+        <p style="color:red;"> <?= $this->session->flashdata('error'); ?> </p>
+    <?php endif; ?>
+      <form action="<?= site_url('auth/process_login') ?>" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" name="username" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -38,7 +40,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control"name="password" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -86,11 +88,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js')?>"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js')?>"></script>
 
 </body>
 </html>
